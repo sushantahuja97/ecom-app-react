eeba88078cf14c9b435dbccdcf1a955483d99cd2
sushant <ahujasushant97@gmail.com>
2020-03-04 16:39:47 +0530

    added reselect lib, updated cart-dropdown and cart-item with reselect and memoized solution

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-icon.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
@@ -19,8 +20,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 )
 
-const mapStateToProps = ({ cart: {cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
